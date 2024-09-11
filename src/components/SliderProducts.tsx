fbcecae92adb5e144a9bf2d6ca232e9c737db15c
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-09-11 19:47:44 +0300

    update

--- src/components/SliderProducts.tsx
+++ src/components/SliderProducts.tsx
@@ -6,7 +6,7 @@ const ProductsSlider = ({ type }: { type: ProductType }) => {
   return (
     <div className="mx-auto my-24  containerWrapper">
       <div className="flex flex-col text-center gap-2 items-center mb-10 text-slate-700  ">
-        <h3 className="font-bold font-serif  text-[30px] sm:text-[50px]">
+        <h3 className="font-bold font-serif capitalize  text-[30px] sm:text-[50px]">
           {type} products
         </h3>
         <p className="text-[16px] sm:text-[25px] font-semibold text-slate-800 max-w-[800px]">
