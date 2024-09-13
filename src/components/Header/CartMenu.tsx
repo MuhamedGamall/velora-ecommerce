d324a17ee117c0ee66b284c70208db04a7838f96
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-09-13 18:34:16 +0300

    restore

---
+++ src/components/Header/CartMenu.tsx
@@ -0,0 +1,28 @@
+import useShoppingBagStore from "@/zustand/store/cartStore";
+import { useEffect } from "react";
+import PreviewMyProducts from "../PreviewMyProducts";
+
+export default function CartMenu({}: {}) {
+  const {
+    shoppingBag,
+    fetchShoppingBag,
+    resetShoppingBag,
+    onOpen,
+    onClose,
+    isOpen,
+  } = useShoppingBagStore();
+  useEffect(() => {
+    fetchShoppingBag();
+  }, []);
+
+  return (
+    <PreviewMyProducts
+      type="cart"
+      data={shoppingBag}
+      onClose={onClose}
+      isOpen={isOpen}
+      onOpen={onOpen}
+      resetBag={resetShoppingBag}
+    />
+  );
+}
